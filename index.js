2c1b8b3c7db62d032164fd0577e93d29f83904e1
Rush <kriscookies20@gmail.com>
2022-06-24 11:19:56 -0700

    progess on process of code working

--- index.js
+++ index.js
@@ -1,13 +1,59 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMark = require('./utils/generateMarkdown.js');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: "What is the title of your project?", 
+     },
+     {
+        type: 'input',
+        name: 'username',
+        message: "What is your GitHub username?",
+     },
+     {
+        type: 'input',
+        name: 'email',
+        message: "What is your email address?",
+     }
+    // // title of project
+    // "What is the title of your project?",
+    // // Info added to: Description, Installation, Usage, Contributing, & Tests
+    // "Enter a description for your project.",
+    // "Enter installation instructions",
+    // "Enter usage information",
+    // "Enter contribution guidelines",
+    // "Enter test instructions",
+  
+    // // Questions Section: for GitHub username link
+    // "Enter your GitHub username",
+    // "Enter your email address"
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+];
+
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((answers) => {
+        console.log(answers);
+        var template = generateMark(answers)
+        fs.writeFile('README.md', template, (err) => {
+            if(err) {
+                // throw or to return
+                throw err
+            } else {
+                console.log("file create successfully")
+            }
+        })
+        console.log(template);
+    })
+}
 
 // Function call to initialize app
 init();
